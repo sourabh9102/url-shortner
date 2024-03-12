020b0a2c61f1a92f60a5b552f680e18468801829
sourabh9102 <er.sourabhbhatt@gmail.com>
2024-03-12 14:38:21 +0530

    url-shortner frontend view included

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const path = require("path");
 const urlRoute = require("./routes/url");
 const { connectToMongoDB } = require("./db");
 const URL = require("./models/url");
+const staticRoute = require("./routes/statisRouter");
 
 const app = express();
 const PORT = 8001;
@@ -10,11 +12,23 @@ connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
   console.log("MongoDB connected")
 );
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.get("/test", async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render("home", {
+    urls: allUrls,
+  });
+});
+
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
